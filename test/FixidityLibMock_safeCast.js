c1ba6affa67adc84216f37b086496f0f35fcf97d
sam bacha <sam@freighttrust.com>
2021-08-26 23:07:26 -0700

    v1.1.0 (#1)
    Co-authored-by: Bernardo Vieira <bernardo5@live.com.pt>
    Co-authored-by: vsfalliance <>
    Co-authored-by: Alberto Cuesta Cañada <mail@albertocuesta.es>

---
+++ test/FixidityLibMock_safeCast.js
@@ -0,0 +1,54 @@
+const FixidityLibMock = artifacts.require('./FixidityLibMock.sol');
+const BigNumber = require('bignumber.js');
+const chai = require('chai');
+const { itShouldThrow } = require('./utils');
+
+// use default BigNumber
+chai.use(require('chai-bignumber')()).should();
+
+contract('FixidityLibMock - safeCast', () => {
+    let fixidityLibMock;
+    let maxInt256;
+
+    before(async () => {
+        fixidityLibMock = await FixidityLibMock.deployed();
+        maxInt256 = new BigNumber(await fixidityLibMock.maxInt256());
+    });
+
+    describe('safeIntToUint', () => {
+        it('safeIntToUint(0)', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.safeIntToUint(0),
+            );
+            result.should.be.bignumber.equal(0);
+        });
+        it('safeIntToUint(1)', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.safeIntToUint(1),
+            );
+            result.should.be.bignumber.equal(1);
+        });
+        itShouldThrow('safeIntToUint(-1)', async () => {
+            await fixidityLibMock.safeIntToUint(-1);
+        }, 'Cannot cast negative signed integer to unsigned integer.');
+    });
+
+    describe('safeUintToInt', () => {
+        it('safeUintToInt(0)', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.safeUintToInt(0),
+            );
+            result.should.be.bignumber.equal(0);
+        });
+        it('safeUintToInt(1)', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.safeUintToInt(1),
+            );
+            result.should.be.bignumber.equal(1);
+        });
+
+        itShouldThrow('maxInt+1()', async () => {
+            await fixidityLibMock.safeUintToInt(maxInt256.plus(1).toString(10));
+        }, 'Cannot cast overflowing unsigned integer to signed integer.');
+    });
+});
